667b6a2d10d53c748a8dacf8702c6284f81f2ec9
Martin Maldoando <martin.maldonado26@gmail.com>
2023-01-10 09:38:09 -0300

    Eventos en JS

---
+++ fundamentos-javascript/js/function2.js
@@ -0,0 +1,5 @@
+//Eventos en JS
+document.addEventListener('click', function() {
+    console.log("Hola mundo desde EventListener");
+    document.getElementById('demo').innerHTML = "Estamos probando nuestro primer evento en JS";
+})
